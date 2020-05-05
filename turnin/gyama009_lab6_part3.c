75b12b428b59c83610f9f706bb3fd4b6b2041817
Garrett <LuckyGY98@gmail.com>
2020-05-04 21:22:25 -0700

    Second commit

---
+++ turnin/gyama009_lab6_part3.c
@@ -0,0 +1,108 @@
+/*	Author: gyama009
+ *  Partner(s) Name: 
+ *	Lab Section: 022
+ *	Assignment: Lab #6 Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+#include "timer.h"
+
+enum States{Start, Init, Increment, Decrement, Reset} state;
+
+void Tick(){
+	switch(state){
+		case Start: // Initial transition
+			PORTC = 0x07;
+			state = Init;
+			break;
+		case Init:
+			if((PINA & 0x03) == 0x03){
+				state = Reset;
+				PORTC = 0x00;
+			}
+			else if(((PINA & 0x02) == 0x02) && (PORTC > 0)){
+				state = Decrement;
+				PORTC -= 1;
+			}
+			else if(((PINA & 0x01) == 0x01) && (PORTC < 9)){
+				state = Increment;
+				PORTC += 1;
+			}
+			else{
+				state = Init;
+			}
+			break;
+		case Increment:
+			if((PINA | 0x00) == 0x00){
+				state = Init;
+			}
+			else if((PINA & 0x03) == 0x03){
+				state = Reset;
+				PORTC = 0x00;
+			}
+			else{
+				state = Increment;
+			}
+			break;
+		case Decrement:
+			if((PINA | 0x00) == 0x00){
+				state = Init;
+			}
+			else if((PINA & 0x03) == 0x03){
+				state = Reset;
+				PORTC = 0x00;
+			}
+			else{
+				state = Decrement;
+			}
+			break;
+		case Reset:
+			if(((PINA & 0x03) == 0x03) || (PORTC == 0x09) || (PORTC == 0x00)){
+				state = Reset;
+				PORTC = 0x00;
+			}
+			else if(((PINA & 0x01) == 0x01) && (PORTC < 9)){
+				state = Increment;
+				PORTC += 1;
+			}
+			else if(((PINA & 0x02) == 0x02) && (PORTC > 0)){
+				state = Decrement;
+				PORTC -= 1;
+			}
+			else{
+				state = Init;
+			}
+			break;
+		default:
+			state = Start;
+			break;
+	} // Transitions
+	switch(state){ // State actions
+		case Init:
+			break;
+		case Increment:
+			break;
+		case Decrement:
+			break;
+		case Reset:
+			break;
+		default:
+			break;
+	} // State actions
+}
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+	state = Start;
+	while (1) {
+		Tick();	
+	}
+	return 0;
+}
